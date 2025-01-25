3f8f78c8c856b2465cce75e15f2294cd3fdbbd9b
VinayKumar X. Chaudhary <VinayKumar.X.Chaudhary@kp.org>
2025-01-25 15:29:38 +0530

    FizzBuzz - Code task#1 - 25th Jan

---
+++ src/atb10xTasks/LoopsandCondition/atb10x_FizzBuzz.java
@@ -0,0 +1,28 @@
+package atb10xTasks.LoopsandCondition;
+
+
+/*
+ TODO: print 1 to 100
+  :: for multiple of 3 print - Fizz
+  :: for Multiple of 5 print - buzz
+  :: for multiple of both print- Fizzbuzz
+  >> Use for loop and If else statement
+ */
+
+public class atb10x_FizzBuzz {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+}
