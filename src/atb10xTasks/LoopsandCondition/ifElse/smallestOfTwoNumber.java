a6f077e46a6781da9a8a9c636ccea5e7f8b3686b
VinayKumar X. Chaudhary <VinayKumar.X.Chaudhary@kp.org>
2025-01-30 19:30:28 +0530

    smallestOfTwoNumber.java added - 30 june task

---
+++ src/atb10xTasks/LoopsandCondition/ifElse/smallestOfTwoNumber.java
@@ -0,0 +1,28 @@
+package atb10xTasks.LoopsandCondition.ifElse;
+
+import java.util.Scanner;
+
+public class smallestOfTwoNumber {
+
+    public static void main(String[] args) {
+
+        int a, b ;
+
+        Scanner sc =  new Scanner(System.in);
+        System.out.println("Enter first Number");
+        a= sc.nextInt();
+        System.out.println("Enter Second Number");
+        b=sc.nextInt();
+        smallestOfTwo(a,b);
+    }
+
+    public static  void smallestOfTwo (int a, int b){
+         if (a>b){
+             System.out.println( b + " << is Smallest of two numbers");
+         } else{
+             System.out.println( a + " << is Smallest of two numbers");
+
+         }
+
+    }
+}
