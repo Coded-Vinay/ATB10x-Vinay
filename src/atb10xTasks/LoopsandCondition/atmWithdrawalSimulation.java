f20af915bb4f31b9373d590ae4984c22af71c43f
VinayKumar X. Chaudhary <VinayKumar.X.Chaudhary@kp.org>
2025-01-30 20:19:41 +0530

    atmWithdrawalSimulation.java

---
+++ src/atb10xTasks/LoopsandCondition/atmWithdrawalSimulation.java
@@ -0,0 +1,25 @@
+package atb10xTasks.LoopsandCondition;
+
+import java.util.Scanner;
+
+public class atmWithdrawalSimulation {
+    public static void main(String[] args) {
+        int account_Balance = 10000;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter amount to withdraw >> $ ");
+        int withdrawals = sc.nextInt();
+
+        if ((withdrawals > 0) && (withdrawals % 100 == 0) && (withdrawals<10000)) {
+            atmLogic(account_Balance, withdrawals);
+        } else {
+            System.out.print("Please ensure withdrawal amount is multiple of 100");
+        }
+    }
+
+    public static void atmLogic(int account_Balance, int withdrawals) {
+
+        int balance = account_Balance - withdrawals;
+        System.out.print("your Remaining balance is >> $ " + balance);
+    }
+}
