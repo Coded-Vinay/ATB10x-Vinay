e84cc6df31fd0210ff6b3249b5bfd57cac2db854
VinayKumar X. Chaudhary <VinayKumar.X.Chaudhary@kp.org>
2025-02-17 18:25:24 +0530

    space optimisaiton

--- src/atb10xTasks/OOPSConcepts/BookClassProblemStatement/BookClass.java
+++ src/atb10xTasks/OOPSConcepts/BookClassProblemStatement/BookClass.java
@@ -1,15 +1,12 @@
 package atb10xTasks.OOPSConcepts.BookClassProblemStatement;
 
-
 abstract class BookClass {
-
     abstract void getdetails(String name, String author, int price);
 }
 
 class PrintMyBook extends BookClass {
-
     @Override
-    void getdetails( String name, String author, int price) {
-        System.out.print ( name + "  " + author + "   " + price);
+    void getdetails(String name, String author, int price) {
+        System.out.print(name + "  " + author + "   " + price);
     }
 }
